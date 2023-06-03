a6a7f4fc0d271f8b5a768cddce5d9649f09848cc
Yaroslav-Slava <130373215+Yaroslav-Slava@users.noreply.github.com>
2023-06-03 13:31:24 +0300

    main_reg5

---
+++ Bread_shop/app/src/main/java/com/example/bread_shop/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.bread_shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private EditText usernameInput, phoneInput, passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        phoneInput = (EditText) findViewById(R.id.login_phone_input);
+        passwordInput = (EditText) findViewById(R.id.login_password_input);
+    }
+}
--- Bread_shop/app/src/main/java/com/example/bread_shop/MainActivity.java
+++ Bread_shop/app/src/main/java/com/example/bread_shop/MainActivity.java
@@ -14,8 +14,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button joinButton, loginButton;
-    private ProgressDialog loadingBar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
         joinButton = (Button) findViewById(R.id.main_join_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
-        loadingBar = new ProgressDialog(this);
-
-
-        Paper.init(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
-
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,61 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
-        String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey);
 
-        if(UserPhoneKey != "" && UserPasswordKey != ""){
-            if(!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey) ){
-                ValidateUser(UserPhoneKey,UserPasswordKey);
-
-                loadingBar.setTitle("Вход в приложение");
-                loadingBar.setMessage("Пожалуйста, подождите...");
-                loadingBar.setCanceledOnTouchOutside(false);
-                loadingBar.show();
-            }
-        }
-    }
-
-    private void ValidateUser(final String phone, final String password) {
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
-
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.child("Users").child(phone).exists())
-                {
-                    Users usersData = dataSnapshot.child("Users").child(phone).getValue(Users.class);
-
-                    if(usersData.getPhone().equals(phone))
-                    {
-                        if(usersData.getPassword().equals(password))
-                        {
-                            loadingBar.dismiss();
-                            Toast.makeText(MainActivity.this, "Успешный вход!", Toast.LENGTH_SHORT).show();
-
-                            Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                            Prevalent.currentOnlineUser = usersData;
-                            startActivity(homeIntent);
-                        }
-                        else {
-                            loadingBar.dismiss();
-                        }
-                    }
-                }
-                else {
-                    loadingBar.dismiss();
-                    Toast.makeText(MainActivity.this, "Аккаунт с номером " + phone + "не существует", Toast.LENGTH_SHORT).show();
-
-                    Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                    startActivity(registerIntent);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
     }
 }
